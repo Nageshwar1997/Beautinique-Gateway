75f544101c93e8ce306e5c7d2cb5775d7d8f5afe
Nageshwar <nageshpawarpatil@gmail.com>
2026-04-28 01:26:30 +0530

    Remove unused logoutRouter export from token.route.ts

--- src/modules/user/routes/auth-routes/token.route.ts
+++ src/modules/user/routes/auth-routes/token.route.ts
@@ -3,7 +3,6 @@ import { Router } from 'express';
 import { refreshAccessTokenController } from '../../controllers';
 import { authenticate, tryCatch } from '@/middlewares';
 
-export const logoutRouter = Router();
 export const tokenRouter = Router();
 
 const { token } = GATEWAY_METHODS_AND_PATHS.user.auth;
